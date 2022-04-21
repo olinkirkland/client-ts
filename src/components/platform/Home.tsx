58260e165c8062545f9c24f17be9e44a7b72bd17
Olin Kirkland <me@olinkirk.land>
2022-04-21 14:14:17 +0200

    Layout & Styling the Home page

--- src/components/platform/Home.tsx
+++ src/components/platform/Home.tsx
@@ -9,32 +9,50 @@ export default function Navbar() {
 
   return (
     <div className="home">
-      <div className="home-grid">
-        <HomePanel
-          onClick={() => {
-            Terminal.log('🖱️ Clicked a HomePanel');
-          }}
-          titleText="Don't Fall Quick Play"
-          buttonText="Play Now"
-          image="assets/images/abstract-1.png"
-          big={true}
-        />
-        <HomePanel
-          onClick={() => {
-            Terminal.log('🖱️ Clicked a HomePanel');
-          }}
-          titleText="Game Rules"
-          buttonText="Learn how to play"
-          image="assets/images/abstract-2.png"
-        />
-        <HomePanel
-          onClick={() => {
-            Terminal.log('🖱️ Clicked a HomePanel');
-          }}
-          titleText="Customize avatar"
-          buttonText="Customize"
-          image="assets/images/abstract-3.png"
-        />
+      <div className="home-container">
+        <div className="home-grid">
+          <HomePanel
+            onClick={() => {
+              Terminal.log('🖱️ Clicked a HomePanel');
+            }}
+            titleText="Don't Fall Quick Play"
+            buttonText="Play Now"
+            image="assets/images/abstract-1.png"
+            big={true}
+          />
+          <HomePanel
+            onClick={() => {
+              Terminal.log('🖱️ Clicked a HomePanel');
+            }}
+            titleText="Game Rules"
+            buttonText="Learn how to play"
+            image="assets/images/abstract-2.png"
+          />
+          <HomePanel
+            onClick={() => {
+              Terminal.log('🖱️ Clicked a HomePanel');
+            }}
+            titleText="Customize avatar"
+            buttonText="Customize"
+            image="assets/images/abstract-3.png"
+          />
+          <HomePanel
+            onClick={() => {
+              Terminal.log('🖱️ Clicked a HomePanel');
+            }}
+            titleText="Host a public or private game"
+            buttonText="Host a game"
+            image="assets/images/abstract-4.png"
+          />
+          <HomePanel
+            onClick={() => {
+              Terminal.log('🖱️ Clicked a HomePanel');
+            }}
+            titleText="View open games"
+            buttonText="Open games"
+            image="assets/images/abstract-5.png"
+          />
+        </div>
       </div>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './assets/css/general.css';
 import './assets/css/styles.css';
+import './assets/css/queries.css';
 
 ReactDOM.render(
   <React.StrictMode>
