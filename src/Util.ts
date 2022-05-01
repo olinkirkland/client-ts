9e846dd9e8c3aeddff8ea616d2e1f1437de3b207
Olin Kirkland <me@olinkirk.land>
2022-05-01 12:44:51 +0200

    Register validation

--- src/Util.ts
+++ src/Util.ts
@@ -25,3 +25,8 @@ export function validateEmail(email: string): boolean {
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
 }
+
+export function validatePassword(password: string): boolean {
+  // Validate password
+  return password.length >= 8;
+}
