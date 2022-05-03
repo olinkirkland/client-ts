240bc6419e324fd1ad4e27c048eda007aa4e0da3
Olin Kirkland <me@olinkirk.land>
2022-05-03 17:27:42 +0200

    Fix home-container

--- src/components/popovers/PopoverFriends.tsx
+++ src/components/popovers/PopoverFriends.tsx
@@ -1,3 +1,3 @@
 export default function PopoverFriends() {
-  return <div className="popover friends">FRIENDS POPOVER</div>;
+  return <div className="popover popover-friends">FRIENDS POPOVER</div>;
 }
