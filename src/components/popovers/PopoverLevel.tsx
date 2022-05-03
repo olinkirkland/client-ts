e81b1517794563d64322052d6d787ff1877e1912
Olin Kirkland <me@olinkirk.land>
2022-05-03 17:23:50 +0200

    Add gold popover

--- src/components/popovers/PopoverLevel.tsx
+++ src/components/popovers/PopoverLevel.tsx
@@ -1,3 +1,28 @@
+import Connection from '../../connection/Connection';
+import { experienceNeededFromLevel } from '../../Util';
+import ProgressBar from '../platform/ProgressBar';
+
 export default function PopoverLevel() {
-  return <div className="popover level">LEVEL POPOVER</div>;
+  const me = Connection.instance.me!;
+
+  return (
+    <div className="popover popover-level">
+      <div className="level-badge">
+        <span className="level">{me.level}</span>
+      </div>
+      <span className="v-group">
+        <ProgressBar
+          percent={Math.min(
+            me.experience! / experienceNeededFromLevel(me.level!)
+          )}
+        />
+        <span>{`${me.experience}/${experienceNeededFromLevel(
+          me.level!
+        )} XP`}</span>
+      </span>
+      <p>
+        Earn experience and level up by playing games and completing challenges.
+      </p>
+    </div>
+  );
 }
