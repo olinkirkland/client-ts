d90420098b0ef6a6a8d128db844075fb103f000b
olinkirkland <plakat@live.com>
2022-05-08 23:06:52 +0200

    Join game popup progress

--- src/components/platform/Home.tsx
+++ src/components/platform/Home.tsx
@@ -119,11 +119,7 @@ export default function Home() {
           />
           <HomePanel
             onClick={() => {
-              PopupMediator.open(PopupJoinGame, {
-                onConfirm: () => {
-                  console.log('confirm');
-                }
-              });
+              PopupMediator.open(PopupJoinGame);
             }}
             titleText="View open games"
             buttonText="Join a game"
--- src/components/popups/PopupJoinGame.tsx
+++ src/components/popups/PopupJoinGame.tsx
@@ -2,37 +2,62 @@ import Modal from 'react-modal';
 import React from 'react';
 import { PopupProps } from 'react-popup-manager';
 import { rootElement } from '../../index';
+import axios from 'axios';
+import Connection, { url } from '../../connection/Connection';
 
 type State = {
-  gameId: string | null;
+  games: any[] | null;
+  selectedGame: any | null;
 };
 
-interface PopupJoinGameProps extends PopupProps {
-  onConfirm: (gameId: string) => void;
-}
+interface PopupJoinGameProps extends PopupProps {}
 
 export class PopupJoinGame extends React.Component<PopupJoinGameProps> {
   public readonly state: State = {
-    gameId: null
+    games: null,
+    selectedGame: null
   };
 
   constructor(props: PopupJoinGameProps) {
     super(props);
-    console.log('init');
+    this.refreshList();
+  }
+
+  private refreshList() {
+    axios.get(url + 'game/list').then((res) => {
+      const data = res.data;
+      this.setState({ games: data });
+    });
   }
 
   render() {
-    const { isOpen, onConfirm, onClose } = this.props;
+    const { isOpen, onClose } = this.props;
     return (
       <Modal isOpen={isOpen!} appElement={rootElement!} className="modal">
         <div className="popup">
           <div className="popup-header">
             <span>Join a Game</span>
-            <button className="button-close" onClick={onClose}>
-              <i className="fas fa-times" />
-            </button>
+            <div className="h-group">
+              <button
+                className="button-close"
+                onClick={() => this.refreshList()}
+              >
+                <i className="fas fa-sync"></i>
+              </button>
+              <button className="button-close" onClick={onClose}>
+                <i className="fas fa-times" />
+              </button>
+            </div>
           </div>
-          <div className="popup-content"></div>
+          <div className="popup-content">
+            <ul>
+              {this.state.games?.map((game, index) => (
+                <li key={index}>
+                  <pre>{JSON.stringify(game, null, 2)}</pre>
+                </li>
+              ))}
+            </ul>
+          </div>
           <div className="popup-taskbar">
             <button
               onClick={() => {
@@ -43,7 +68,7 @@ export class PopupJoinGame extends React.Component<PopupJoinGameProps> {
             </button>
             <button
               onClick={() => {
-                onConfirm!(this.state.gameId!);
+                Connection.instance.joinGame(this.state.selectedGame!.id);
                 onClose!();
               }}
             >
