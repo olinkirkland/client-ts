7d41f176e87c8479a9c1be40cdd5f6df31566411
Olin Kirkland <me@olinkirk.land>
2022-05-09 16:28:35 +0200

    Invalidate game data progress

--- src/components/game/GameScreen.tsx
+++ src/components/game/GameScreen.tsx
@@ -1,19 +1,33 @@
 import { useEffect, useState } from 'react';
-import Connection, { ConnectionEventType } from '../../connection/Connection';
+import Connection from '../../connection/Connection';
+import Game from '../../connection/Game';
 
 export default function GameScreen() {
+  const game: Game = Connection.instance.game!;
+
+  const [mode, setMode] = useState(game.mode);
+
   useEffect(() => {}, []);
 
   return (
     <div className="game">
       Game Screen
-      <button
-        onClick={() => {
-          Connection.instance.leaveGame();
-        }}
-      >
-        Leave game
-      </button>
+      <div className="h-group">
+        <button
+          onClick={() => {
+            Connection.instance.startGame();
+          }}
+        >
+          Start Game
+        </button>
+        <button
+          onClick={() => {
+            Connection.instance.leaveGame();
+          }}
+        >
+          Leave game
+        </button>
+      </div>
     </div>
   );
 }
